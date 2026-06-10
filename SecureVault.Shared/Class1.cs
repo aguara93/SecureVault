7382976375c018a66318bbddb8469eef82b7971a
aguara93 <ag.daleki@gmail.com>
2026-06-10 22:20:13 +0200

    add project structure (.Shared, .Client, .Tests)

---
+++ SecureVault.Shared/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SecureVault.Shared
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SecureVault.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace SecureVault.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
