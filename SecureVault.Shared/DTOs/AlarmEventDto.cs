cd7dc762e4ea22c83af65d2b2fe61036b247a7bf
aguara93 <ag.daleki@gmail.com>
2026-06-12 22:47:10 +0200

    add SensorReadingDto and AlarmEventDto

---
+++ SecureVault.Shared/DTOs/AlarmEventDto.cs
@@ -0,0 +1,22 @@
+﻿using SecureVault.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureVault.Shared.DTOs
+{
+    internal class AlarmEventDto
+    {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public string SensorName { get; set; } = string.Empty;
+        public string SensorLocation { get; set; } = string.Empty;
+        public SensorType SensorType { get; set; }
+        public AlarmStatus Status { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime TriggeredAt { get; set; }
+        public DateTime? ResolvedAt { get; set; }
+    }
+}
---
+++ SecureVault.Shared/DTOs/SensorReadingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureVault.Shared.DTOs
+{
+    internal class SensorReadingDto
+    {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public double Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+    }
+}
