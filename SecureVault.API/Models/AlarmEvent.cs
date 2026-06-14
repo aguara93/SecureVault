8abb05d65b0a7a1004c0e0cb1ccdd1cd8f598d07
aguara93 <ag.daleki@gmail.com>
2026-06-14 21:10:35 +0200

    add SensorReading, AlarmEvent and User in .API/Models

---
+++ SecureVault.API/Models/AlarmEvent.cs
@@ -0,0 +1,17 @@
+﻿using SecureVault.Shared.Enums;
+
+namespace SecureVault.API.Models
+{
+    public class AlarmEvent
+    {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public AlarmStatus Status { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime TriggeredAt { get; set; }
+        public DateTime? ResolvedAt { get; set; }
+
+        // Navigation property
+        public Sensor Sensor { get; set; } = null!;
+    }
+}
---
+++ SecureVault.API/Models/SensorReading.cs
@@ -0,0 +1,14 @@
+﻿namespace SecureVault.API.Models
+{
+    public class SensorReading
+    {
+        public int Id { get; set; }
+        public int SensorId { get; set; }
+        public double Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+
+        // Navigation property
+        public Sensor Sensor { get; set; } = null!;
+    }
+}
