c04768728ff64f30a40ee8328ddc514343ade483
aguara93 <ag.daleki@gmail.com>
2026-06-14 21:32:21 +0200

    add SecureVaultDbContext

---
+++ SecureVault.API/Data/SecureVaultDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureVault.API.Models;
+
+namespace SecureVault.API.Data
+{
+    public class SecureVaultDbContext : DbContext
+    {
+        public SecureVaultDbContext(DbContextOptions<SecureVaultDbContext> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Sensor> Sensors { get; set; } = null!;
+        public DbSet<SensorReading> SensorReadings { get; set; } = null!;
+        public DbSet<AlarmEvent> AlarmEvents { get; set; } = null!;
+        public DbSet<User> Users { get; set; } = null!;
+
+    }
+}
