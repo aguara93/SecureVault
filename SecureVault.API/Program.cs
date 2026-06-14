4a04401491f14452dd76e813102c1cf88355174a
aguara93 <ag.daleki@gmail.com>
2026-06-14 22:50:21 +0200

    add DbContext configuration i Program.cs and PostgreSQL connection string i appsettings.json

--- SecureVault.API/Program.cs
+++ SecureVault.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using SecureVault.API.Data;
 
 namespace SecureVault.API
 {
@@ -8,11 +10,17 @@ namespace SecureVault.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Add DbContext
+            builder.Services.AddDbContext<SecureVaultDbContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+           
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
