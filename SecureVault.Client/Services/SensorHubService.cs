270b5a373ae76b2ae2bf381a0f88f1243c5efa5d
aguara93 <ag.daleki@gmail.com>
2026-09-04 22:27:49 +0200

    fix: move SignalR.Client package from .API to .Client project; add SensorHubService

---
+++ SecureVault.Client/Services/SensorHubService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SecureVault.Shared.DTOs;
+
+namespace SecureVault.Client.Services
+{
+    public class SensorHubService : IAsyncDisposable
+    {
+        private HubConnection? _hubConnection;
+
+        public event Action<SensorReadingDto>? OnReadingReceived;
+
+        public async Task StartAsync(string apiBaseUrl)
+        {
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl($"{apiBaseUrl}/hubs/sensor")
+                .WithAutomaticReconnect()
+                .Build();
+
+            _hubConnection.On<SensorReadingDto>("ReceiveSensorReading", (reading) =>
+            {
+                OnReadingReceived?.Invoke(reading);
+            });
+
+            await _hubConnection.StartAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_hubConnection is not null)
+            {
+                await _hubConnection.DisposeAsync();
+            }
+        }
+    }
+}
