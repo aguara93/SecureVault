c3a15a5b0f3c0f5224accdecf03e960f20b097d0
aguara93 <ag.daleki@gmail.com>
2026-09-23 21:08:05 +0200

    docs: add XML documentation comment to SensorHub

--- SecureVault.API/Hubs/SensorHub.cs
+++ SecureVault.API/Hubs/SensorHub.cs
@@ -2,9 +2,17 @@
 
 namespace SecureVault.API.Hubs
 {
+    /// <summary>
+    /// SignalR hub used to push real-time notifications to connected
+    /// clients whenever a new sensor reading is recorded. This hub
+    /// currently defines no server-side methods; it is used solely as
+    /// a broadcast channel from <see cref="Controllers.SensorReadingsController"/>
+    /// to connected Blazor clients.
+    /// </summary>
     public class SensorHub : Hub
     {
-        // doesn't need to implement any methods for now,
-        // just broadcasting messages to connected clients
+        // This hub doesn't need to implement any methods for now,
+        // it is just used for broadcasting messages to connected
+        // clients.
     }
 }
